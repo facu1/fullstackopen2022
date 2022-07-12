6e5f95c0f67fb2140cd8e9dbd08a7e4a04945744
Facundo David Gonzalez <facundo.gonzalez@devactory.com>
2022-07-12 12:37:46 -0300

    modify the variable definitions of App and modify the other parts (step4)

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -12,10 +12,10 @@ const Content = ({parts}) => (
   </div>
 )
 
-const Total = ({exercises}) => {
+const Total = ({parts}) => {
   let total = 0
-  exercises.forEach(exercisesAmount => {
-    total += exercisesAmount
+  parts.forEach(({exercises}) => {
+    total += exercises
   })
 
   return <p>Number of exercises {total}</p>
@@ -23,26 +23,26 @@ const Total = ({exercises}) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={[part1, part2, part3]} />
-      <Total
-        exercises={[part1.exercises, part2.exercises, part3.exercises]}
-      />
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
