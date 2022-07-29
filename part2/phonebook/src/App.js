7491dcb1ea1b2a090b6860cc29916029b104a3c3
Facundo David Gonzalez <facundo.gonzalez@devactory.com>
2022-07-29 11:22:33 -0300

    implement the addition of a person to phonebook (step1)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,18 +6,34 @@ const App = () => {
   ])
   const [newName, setNewName] = useState('')
 
+  const addPerson = (e) => {
+    e.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleChange = (e) => {
+    setNewName(e.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleChange} />
         </div>
         <div>
-          <buttno type='submit'>add</buttno>
+          <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      {persons.map(({ name }) =>
+        <div key={name}>{ name }</div>
+      )}
     </div>
   );
 }
