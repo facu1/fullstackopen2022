e8c6708d4097af59bbdf19f6531d227ccd62947c
Facundo David Gonzalez <facundo.gonzalez@devactory.com>
2022-08-22 16:58:30 -0300

    define totalLikes function (exercise 4.4 helper functions and unit tests, step2)

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -4,4 +4,10 @@ const dummy = (blogs) => {
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+  if (!blogs.length) return 0
+  if (blogs.length === 1) return blogs[0].likes
+  return blogs.reduce((prev, curr) => prev + curr.likes, 0)
+}
+
+module.exports = { dummy, totalLikes }
