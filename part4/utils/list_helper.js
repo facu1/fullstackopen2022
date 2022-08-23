7055d027e2e4d2c1711578d59f7debb82f4ade4a
Facundo David Gonzalez <facundo.gonzalez@devactory.com>
2022-08-22 23:44:00 -0300

    define favoriteBlog function (exercise 4.5 helper functions and unit tests, step3)

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -10,4 +10,26 @@ const totalLikes = (blogs) => {
   return blogs.reduce((prev, curr) => prev + curr.likes, 0)
 }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+  if (!blogs.length) return 0
+  if (blogs.length === 1) {
+    const { title, author, likes } = blogs[0]
+
+    return { title, author, likes }
+  }
+
+  let favoriteLikes = 0, indexOfFavorite = 0
+
+  for (const [index, blog] of blogs.entries()) {
+    if (blog.likes > favoriteLikes) {
+      favoriteLikes = blog.likes
+      indexOfFavorite = index
+    }
+  }
+
+  const { title, author, likes } = blogs[indexOfFavorite]
+
+  return { title, author, likes }
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
