801c0432845781dc579a7464d43507223acad666
Facundo David Gonzalez <facundo.gonzalez@devactory.com>
2022-09-08 00:42:28 -0300

    implement token-based authentication (exercise 4.18 bloglist expansion, step6)

--- part4/app.js
+++ part4/app.js
@@ -8,6 +8,7 @@ const mongoose = require('mongoose')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const mongoUrl = config.MONGODB_URI
 
@@ -28,6 +29,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
