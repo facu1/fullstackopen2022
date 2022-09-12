d81dde167379fd762df3fcec08af3289527dad77
Facundo David Gonzalez <facundo.gonzalez@devactory.com>
2022-09-12 15:50:23 -0300

    add a button to each blog (exercise 5.7: blog list frontend, step7)

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,34 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const hideStyle = { display : 'none'}
+
+  const toggleExpanded = () => setExpanded(!expanded)
+
+  return (
+    <div style={blogStyle}>
+      <div style={expanded ? hideStyle : {}}>
+        {blog.title} {blog.author}
+        <button onClick={toggleExpanded}>view</button>
+      </div>
+      <div style={expanded ? {} : hideStyle}>
+        <div>{blog.title}<button onClick={toggleExpanded}>view</button></div>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes}<button>like</button></div>
+        <div>{blog.author}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
