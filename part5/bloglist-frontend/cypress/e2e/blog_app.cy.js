fbfa6c3f664651c3a9e8f7d92695fb439eda3118
Facundo David Gonzalez <facundo.gonzalez@devactory.com>
2022-09-23 12:15:47 -0300

    make tests for logging in (exercise 5.18: bloglist end to end testing, step2)

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,11 @@
 describe('Blog app', function() {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST', 'http://localhost:3003/api/users', {
+      username: 'fgonza',
+      password: 'fgonza123',
+      name: 'Facundo Gonzalez'
+    })
     cy.visit('http://localhost:3000')
   })
 
@@ -9,5 +14,23 @@ describe('Blog app', function() {
     cy.get('#username')
     cy.get('#password')
     cy.get('#login-button')
+  })
+
+  describe('Login', async function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('fgonza')
+      cy.get('#password').type('fgonza123')
+      cy.get('#login-button').click()
+      cy.contains('Facundo Gonzalez logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('fgonza')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+      cy.get('.fails')
+        .should('contain', 'wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
