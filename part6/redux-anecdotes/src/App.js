b93580b856cc277e02e0f4430b33dff0388a9621
Facundo David Gonzalez <facundo.gonzalez@devactory.com>
2022-10-03 11:38:27 -0300

    fetch the anecdotes using json-server (exercise 6.13: anecdotes and the backend, step1)

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -2,15 +2,28 @@ import Notification from './components/Notification'
 import Filter from './components/Filter'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
+import { useEffect } from 'react'
+import anecdoteService from './services/anecdote'
+import { useDispatch } from 'react-redux'
+import { setAnecdotes } from './reducers/anecdoteReducer'
 
-const App = () => (
-  <div>
-    <h2>Anecdotes</h2>
-    <Notification />
-    <Filter />
-    <AnecdoteList />
-    <AnecdoteForm />
-  </div>
-)
+const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdoteService.getAll()
+      .then((anecdotes) => dispatch(setAnecdotes(anecdotes)))
+  }, [dispatch])
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <Notification />
+      <Filter />
+      <AnecdoteList />
+      <AnecdoteForm />
+    </div>
+  )
+}
 
 export default App
