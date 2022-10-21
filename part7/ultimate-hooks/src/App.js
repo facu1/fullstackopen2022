1ea98ee15a3844e02dbfcca59e3dab19d29d0407
Facundo David Gonzalez <facundo.gonzalez@devactory.com>
2022-10-21 01:43:37 -0300

    create useResource hook (exercise 7.8: ultimate hooks)

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -18,10 +18,15 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    axios.get(baseUrl)
+      .then((response) => setResources(response.data))
+  }, [baseUrl])
 
-  const create = (resource) => {
-    // ...
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+
+    setResources([...resources, response.data])
   }
 
   const service = {
